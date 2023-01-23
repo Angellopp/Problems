6a776ee7f7f8010d5d2920a56074cacda820886f
Angello <angellojamir@hotmail.com>
2023-01-23 13:39:00 -0500

    Add solution for problem `C - Common Divisors` Link: https://codeforces.com/contest/1203/problem/C Tags: implementation, math, *1300 Ref: https://codeforces.com/contest/1203/submission/190248406

--- codeforces/1203/C.cpp
+++ codeforces/1203/C.cpp
@@ -1,30 +1,20 @@
 #include <bits/stdc++.h>
-#include <numeric>
-
-#define ll long long
- 
+typedef long long int ll;
 using namespace std;
- 
-ll divi (ll n) {
-    ll c = 0;
+ll divi(ll n){
+    ll c=0;
     for (ll d = 1; d*d <= n; ++d) {
         if (n % d == 0) {
             c++;
-            if (d != n/d)  
-                c++;
+            if (d != n/d)  c++;
         }
-    }
-    return c;
+    }return c;
 }
 int main(){
-    int n; 
-    ll aux; 
-    cin >> n >> aux;
-    ll mcd = aux;
-    while (n--) {
-        cin >> aux;
-        mcd = gcd(aux, mcd);
+    ll n,aux; cin>>n>>aux;
+    ll mcd=aux;
+    while(n--){
+        cin>>aux;mcd=__gcd(aux,mcd);
     }
-    cout << divi(mcd);
-    return 0;
+    cout<<divi(mcd);
 }
