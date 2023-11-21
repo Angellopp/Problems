4afa921649308cc14a37263e6d04a84c683e5ad5
Angello <angellojamir@hotmail.com>
2023-11-21 18:45:00 -0500

    Add solution for problem `A - Milica and String` Link: https://codeforces.com/contest/1898/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/1898/submission/233748249

--- codeforces/1898/A.cpp
+++ codeforces/1898/A.cpp
@@ -2,38 +2,42 @@
 using namespace std;
 
 int main() {
-    int tt, n, k, dpa[102]{} ,dpb[103]{};
+    int tt, n, k, dpb[102]{}, dpa[102]{};
     string s;
     cin >> tt;
     while (tt--) {
         cin >> n >> k;
         cin >> s;
+        cin.ignore();
+        // cout << s << "\n";
         int cb = 0;
         for (int i = 1; i <= n; i++) {
-            dpa[i] = dpa[i-1] + (s[i-1] == 'A');
+            dpa[i] = dpa[i-1] + (s[i-1] == 'A'); 
             dpb[i] = dpb[i-1] + (s[i-1] == 'B');
-            if(s[i-1] == 'B') cb++;
+            if (s[i-1] == 'B') cb++;
         }
-        if (cb == k) {
-            cout << "0\n"; continue;
+        // cout << "cb =" <<  cb << " k = " << k << "\n";
+        if (k == cb) {
+            cout << "0\n";
+            continue;
         }
-        cout << "1\n";
-        if (cb < k) {
-            for (int i = 0; i <= n; i++) {
-                if (dpa[i] == k - cb) {
-                    cout << i << " B\n";
+        // cout << "1\n";
+        if (k > cb) {
+            for(int i = 0; i <= n; i++) {
+                if(dpa[i] == k-cb){
+                    cout << "1\n" << i << " B\n"; 
                     break;
                 }
             }
         }
         else {
-            for (int i = 0; i <= n; i++) {
-                if (dpb[i] == cb - k) {
-                    cout << i << " A\n";
+            for(int i = 0; i <= n; i++) {
+                if(dpb[i] == cb-k){
+                    cout << "1\n" << i << " A\n"; 
                     break;
                 }
             }
-
         }
+
     }
 }
