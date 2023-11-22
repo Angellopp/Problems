2682919b4a11757de4e95bb18e4d110682877978
Angello <angellojamir@hotmail.com>
2023-11-22 00:34:00 -0500

    Add solution for problem `B - Milena and Admirer` Link: https://codeforces.com/contest/1898/problem/B Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1898/submission/233764557

--- codeforces/1898/B.cpp
+++ codeforces/1898/B.cpp
@@ -5,13 +5,11 @@ using namespace std;
 int main() {
     int tt, n, a[200002];
     cin >> tt;
-    ll ans;
-    int res = 0, par = 0;
     while (tt--) {
         cin >> n;
         for (int i = 0; i < n; i++) cin >> a[i];
-        ans = 0;
-        res = a[n-1], par = 0;
+        ll ans = 0;
+        ll res = a[n-1], par;
         for (int i = n-2; i >= 0; i--) {
             par = (a[i] + res - 1) / res;
             res = a[i] / par;
