acb0f0bf9a24799caa33917b3be3f7aad15ff356
Angello <angellojamir@hotmail.com>
2023-12-06 16:41:00 -0500

    Add solution for problem `E - Good Triples` Link: https://codeforces.com/contest/1907/problem/E Tags: brute force, combinatorics, number theory, *1600 Ref: https://codeforces.com/contest/1907/submission/236122722

--- codeforces/1907/E.cpp
+++ codeforces/1907/E.cpp
@@ -2,6 +2,16 @@
 #define ll long long
 using namespace std;
 int a[11] = {1,3,6,10,15,21,28,36,45,55};
+
+// int sumaDeCifras(int n) {
+//     int suma = 0;
+//     while(n > 0){
+//         suma += n % 10;
+//         n /= 10;
+//     }
+//     return suma;
+// }
+
 ll ans(ll n) {
     if(n<=9) return a[n];
     return a[n % 10] * 1LL * ans(n/10);
@@ -9,9 +19,23 @@ ll ans(ll n) {
 
 int main(){
     int tt; cin >> tt;
+    // for(int n = 0; n <= 200; n+=10) {
+    //     // int n ;
+    //     long long int cont = 0;
+    //     for (int i = 0; i <= n; i++) {
+    //         for(int j = 0; i+j <= n; j++) {
+    //             if((sumaDeCifras(i) + sumaDeCifras(j) + sumaDeCifras(n-i-j)) == sumaDeCifras(n)) {
+    //                 // cout << i << " " << j << endl;
+    //                 cont++;
+    //             }
+    //         }
+    //     }
+    //     cout << n << ": " << cont << endl;
+    // }
     while (tt--) {
         ll n;
         cin >> n;
+        // if(n == 10000000) cout << 3 << "\n";
         cout << ans(n) << "\n";
     }
 }
