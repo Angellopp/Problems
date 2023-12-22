33231542527a2a7ecc16ae4ee7b5ffd7ef9c0878
Angello <angellojamir@hotmail.com>
2023-12-22 18:12:00 -0500

    Add solution for problem `E1 - Game with Marbles (Easy Version)` Link: https://codeforces.com/contest/1914/problem/E1 Tags: brute force, games, greedy, sortings, *1400 Ref: https://codeforces.com/contest/1914/submission/238429721

--- codeforces/1914/E1.cpp
+++ codeforces/1914/E1.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-const int MAXN = 2*1e5 + 5;
+const int MAXN = 6;
 int main() {
     int tt; 
     cin >> tt;
     ll a[MAXN], b[MAXN];
+    // vector <pair<ll,int>> cc(MAXN);
     while (tt--) {
         vector <pair<ll,int>> cc;
         int n;
@@ -14,11 +15,15 @@ int main() {
         for (int i = 0; i < n; i++) cin >> a[i]; 
         for (int i = 0; i < n; i++) cin >> b[i];
         for (int i = 0; i < n; i++) {
-            cc.push_back({(a[i] + b[i]), i});
+            a[i] = (a[i] - 1);
+            b[i] = (1 - b[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            cc.push_back({(a[i] - b[i]), i});
         }
         sort(cc.rbegin(), cc.rend());
         for (int i = 0; i < n; i++) {
-            ans += (i % 2) ? 1-b[cc[i].second] : a[cc[i].second]-1;
+            ans += (i % 2) ? b[cc[i].second] : a[cc[i].second];
         }
         cout << ans <<"\n";
 
