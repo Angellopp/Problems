20b80a87ee785b7ef77a186c565875f45eff47e4
Angello <angellojamir@hotmail.com>
2023-12-28 17:16:00 -0500

    Add solution for problem `C - Game with Multiset` Link: https://codeforces.com/contest/1913/problem/C Tags: binary search, bitmasks, brute force, greedy, *1300 Ref: https://codeforces.com/contest/1913/submission/239425668

--- codeforces/1913/C.cpp
+++ codeforces/1913/C.cpp
@@ -4,19 +4,28 @@ using namespace std;
 int main() {
     int tt;
     cin >> tt;
-    int bitmap[29+5]{}, bitaux[29+5]{};
-    int c, n, aux;
+    int bitmap[29+5]{};
+    int c, n;
     while (tt--) {
         cin >> c >> n;
-        if (c == 1) 
+        if (c == 1) {
             bitmap[30-n]++;
+        }
         else {
+            int bitaux[29+5];
             for (int i = 0; i < 31; i++) bitaux[i] = bitmap[i];
+            
+            // for(int i = 0; i < 31; i++) cout << bitaux[i] << " ";
+            // cout << "\n";
+
+            int aux = 0;
             for (int i = 1; i < 31; i++) {
                 ((n>>(i-1)) & 1) ? aux = (bitaux[31-i] - 1) / 2 : aux = bitaux[31-i] / 2;
                 bitaux[31-i] -= 2*aux;
                 bitaux[30-i] += aux;
             }
+            // for(int i = 0; i < 31; i++) cout << bitaux[i] << " ";
+            // cout << "\n";
             bool b = true;
             for (int i = 1; i < 31; i++) {
                 if (bitaux[31-i] < ((n>>(i-1)) & 1)) {
