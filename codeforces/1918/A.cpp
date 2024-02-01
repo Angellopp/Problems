09eda0895e4db1facd98b17ca88e86b60a7bff98
Angello <angellojamir@hotmail.com>
2024-02-01 12:48:00 -0500

    Add solution for problem `A - Brick Wall` Link: https://codeforces.com/contest/1918/problem/A Tags: constructive algorithms, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1918/submission/244447008

---
+++ codeforces/1918/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main() {
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int n, m;
+        cin >> n >> m;
+        cout << n * (m / 2) << " \n";
+    }
+    return 0;
+}
