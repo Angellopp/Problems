a802dcf3c182b91cc49e5f170a8613f0afec1547
Angello <angellojamir@hotmail.com>
2024-02-02 18:22:00 -0500

    Add solution for problem `C - XOR-distance` Link: https://codeforces.com/contest/1918/problem/C Tags: bitmasks, greedy, implementation, math, *1400 Ref: https://codeforces.com/contest/1918/submission/244620169

--- codeforces/1918/C.cpp
+++ codeforces/1918/C.cpp
@@ -18,34 +18,41 @@ int main() {
         while ((r>>i & 1LL) == 0 and i >= 0) {
             ans += (a & (1LL<<i));
             ans -= (b & (1LL<<i));
+            a -= a & (1LL<<i);
+            b -= b & (1LL<<i);
             i--;
         }
+        ll d = 0;
         if (ans > 0) {
-            ll aux = 0;
+            bool can = 0;
             while(i >= 0) {
-                ll dif = (1LL<<i & a) - (1LL<<i & b);
-                if (dif) {
-                    if (dif > 0 and (aux | 1LL<<i) <= r){
-                        aux |= 1LL<<i;
-                        ans -= dif;
+                ll d1 = (1LL<<i & a) - (1LL<<i & b);
+                ll d2 = -d1;
+                if (can) d += min(d1, d2);
+                else {
+                    if ((r>>i & 1LL) == 1) {
+                        if (d2 >= d1) can = 1;
+                        d += min(d1, d2);
                     }
-                    else ans += dif;
+                    else d += d1;
                 }
                 i--;
             }
         }
         else {
+            if (a < b) swap(a, b);
             ll can = 1;
             while(i >= 0) {
-                ll d = (1LL<<i & a) - (1LL<<i & b);
-                if (d) {
-                    ans += can * abs(d);
+                ll d1 = (1LL<<i & a);
+                ll d2 = (1LL<<i & b);
+                if (d1 ^ d2) {
+                    ans += can * abs(d1 - d2);
                     can = -1;
                 }
                 i--;
             }
         }
-        cout << ans << "\n";
+        cout << ans + d << "\n";
     }
     return 0;
 }
