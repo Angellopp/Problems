85cf9128e4cb11349ce96379e73523206a6822f2
Angello <angellojamir@hotmail.com>
2024-02-06 12:45:00 -0500

    Add solution for problem `D - Find the Different Ones!` Link: https://codeforces.com/contest/1927/problem/D Tags: binary search, brute force, data structures, dp, dsu, greedy, two pointers, *1300 Ref: https://codeforces.com/contest/1927/submission/245238895

--- codeforces/1927/D.cpp
+++ codeforces/1927/D.cpp
@@ -1,50 +1,34 @@
 #include <bits/stdc++.h>
-#define ll long long
+ 
 using namespace std;
-
-
+using ll = long long;
+ 
+void solve() {
+    int n, q;
+    cin >> n;
+    vector<int> a(n), p(n, -1);
+    cin >> a[0];
+    for (int i = 1; i < n; ++i) {
+        cin >> a[i];
+        p[i] = (a[i - 1] != a[i]) ? i - 1 : p[i - 1];
+    }
+    cin >> q;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        l -= 1, r -= 1;
+        if(p[r] >= l) cout << p[r] + 1 << " " << r + 1 << '\n';
+        else cout << "-1 -1\n";
+    }
+}
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int tt;
-    cin >> tt;
-
-    while (tt--){
-        int n;
-        cin >> n;
-        vector <int> a(n+2);
-        for (int i = 1; i <= n; i++) cin >> a[i];
-        vector<pair<int , int>> p;
-        int curr = a[1], ini = 1;
-        for (int i = 2; i <= n; i++) {
-            if(a[i] != curr) {
-                p.push_back({curr, ini});
-                curr = a[i];
-                ini = i;
-            }
-        }
-        p.push_back({curr, ini});
-        vector <int> ind;
-        for (int i = 0; i < p.size(); i++) {
-            ind.push_back(p[i].second);
-        }
-        int q, l ,r;
-        cin >> q;
-        while (q--) {
-            cin >> l >> r;
-            auto indl = upper_bound(ind.begin(), ind.end(), l);
-            auto indr = upper_bound(ind.begin(), ind.end(), r);
-            if (indl != ind.begin()) --indl;
-            if (indr != ind.begin()) --indr;
-            int idl = indl - ind.begin();
-            int idr = indr - ind.begin();
-            if(idl == idr) cout << "-1 -1\n";
-            else {
-                if(p[idl].first == p[idr].first) idr--;
-                cout << l << " " << p[idr].second << "\n";
-            }
-        }
-        cout << "\n";
+    int test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
     }
     return 0;
 }
