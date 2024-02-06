0a758b5c0de79697b5b81c30b52bf0fe3ab4ed78
Angello <angellojamir@hotmail.com>
2024-02-06 10:32:00 -0500

    Add solution for problem `C - Choose the Different Ones!` Link: https://codeforces.com/contest/1927/problem/C Tags: brute force, greedy, math, *1000 Ref: https://codeforces.com/contest/1927/submission/245152494

--- codeforces/1927/C.cpp
+++ codeforces/1927/C.cpp
@@ -13,7 +13,7 @@ int main() {
         int n, m, k;
         cin >> n >> m >> k;
         vector <int> a(n), b(m);
-        vector <bool> c(k+1), d(k+1), comp(k+1);
+        vector <int> c(k+1), d(k+1), comp(k+1);
         for (int i = 0; i < n; i++){
             cin >> a[i];
             if (a[i] <= k){
@@ -29,16 +29,30 @@ int main() {
             }
         }
         int sumc = 0, sumd = 0;
-        for (int i = 1; i <= k; i++) sumc += c[i];
-        for (int i = 1; i <= k; i++) sumd += d[i];
+        for (int i = 1; i <= k; i++) {
+            sumc += c[i];
+        }
+        for (int i = 1; i <= k; i++) {
+            sumd += d[i];
+        }
         bool can = 1;
         for (int i = 1; i <= k; i++) {
             if (comp[i] == 0){
+                cout << "NO\n";
                 can = 0;
                 break;
             }
         }
-        cout << ((can and sumc >= k/2 and sumd >= k/2) ? "YES\n" : "NO\n");   
+        if (can){
+        if (sumc >= k/2 and sumd >= k/2){
+            cout << "YES\n";
+        }
+        else cout << "NO\n";
+            
+        } 
+        // cout << 1;
+
+        
     }
     return 0;
 }
