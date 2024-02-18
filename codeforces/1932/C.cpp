fd3ca4a9754d4c7beb2acac78ac9e6aeb00d01d6
Angello <angellojamir@hotmail.com>
2024-02-18 09:50:00 -0500

    Add solution for problem `C - LR-remainders` Link: https://codeforces.com/contest/1932/problem/C Tags: brute force, data structures, implementation, math, two pointers Ref: https://codeforces.com/contest/1932/submission/247103016

--- codeforces/1932/C.cpp
+++ codeforces/1932/C.cpp
@@ -1,31 +1,39 @@
-    #include <bits/stdc++.h>
-    #define ll long long
-    using namespace std;
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
-    int main(){
-        int tt;
-        cin >> tt;
-        vector <ll> v(200005);
-        while (tt--) {
-            int n, k;
-            cin >> n >> k;
-            for (int i = 0; i < n; i++) cin >> v[i];
-            string s;
-            cin >> s;
-            int l = -1;
-            for (int i = 0; i < n; i++) {
-                if (s[i] == 'L') l++;
-            }
-            if (s[n-1] == 'R') l++;
-            vector <ll> ans;
-            ans.emplace_back(v[l]%k);
-            int xdl = l, xdr = l;
-            for (int i = n-2; i >= 0; i--) {
-                int ind = ((s[i] == 'R') ? ++xdr : --xdl);
-                ans.emplace_back((ans.back()*v[ind])%k);
-            }
-            for (int i = ans.size()-1; i >= 0; i--)
-                cout << ans[i] << " ";
-            cout << "\n";
+int main(){
+    int tt;
+    cin >> tt;
+    vector <ll> v(200005);
+    while (tt--) {
+        int n, k;
+        cin >> n >> k;
+
+        for (int i = 0; i < n; i++) cin >> v[i];
+        string s;
+        cin >> s;
+        int l = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'L') l++;
         }
+        if (s[n-1] == 'R') l++;
+        l--;
+        vector <ll> ans;
+        ans.emplace_back(v[l]%k);
+        int xdl = l, xdr = l;
+
+        for (int i = n-2; i >= 0; i--) {
+            if (s[i] == 'R') {
+                ans.emplace_back((ans.back()*v[++xdr])%k);
+            }
+            else {
+                ans.emplace_back((ans.back()*v[--xdl])%k);
+            }
+        }
+        for (int i = ans.size()-1; i >= 0; i--) {
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
     }
+}
