265a41459b7198d31501164317ebfc6f8cb7523e
Angello <angellojamir@hotmail.com>
2024-02-22 20:26:00 -0500

    Add solution for problem `D - Flipper` Link: https://codeforces.com/contest/1833/problem/D Tags: brute force, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1833/submission/247812796

---
+++ codeforces/1833/D.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(false); cin.tie (NULL)
+#define rep(ini, n) for (int i = ini; i < n; i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.end()
+#define pb push_back
+#define pf push_front
+#define PI acos(-1)
+#define F first
+#define S second
+
+using u32 = uint32_t;
+using u64 = uint64_t;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<string> vs;
+typedef priority_queue<int> pq_max;
+typedef priority_queue<int, vi, greater<int>> pq_min;
+
+const int maxn = 2e5;
+const u32 MOD = 1000000007;
+
+vector <int> prim;
+int main(){ 
+    fast_io;
+    int tt;
+    cin >> tt;
+    vector <int> v(2000);
+    while (tt--) {
+        int n;
+        cin >> n;
+        int xd1 = 0, xd2 = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+            if (v[i] == n) xd1 = i;
+            if (v[i] == n-1) xd2 = i;
+        }
+        if (xd1 == 0) xd1 = xd2;
+        if (xd1 == n-1) xd1 = n;
+        xd2 = --xd1 - 1;
+        while (xd2 >= 0 and v[xd2] > v[0]) xd2--;
+        for (int i = xd1 + 1; i < n; i++) cout << v[i] << " ";
+        for (int i = xd1; i > xd2; i--) cout << v[i] << " ";
+        for (int i = 0; i <= xd2; i++) cout << v[i] << " ";
+        cout << "\n";
+    }
+    return 0;
+}
