012f5fdc99180408d3d5fcfb300202f43534d8bc
Angello <angellojamir@hotmail.com>
2024-02-28 12:28:00 -0500

    Add solution for problem `C - Largest Subsequence` Link: https://codeforces.com/contest/1905/problem/C Tags: greedy, strings, *1400 Ref: https://codeforces.com/contest/1905/submission/248788111

---
+++ codeforces/1905/C.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false); cin.tie (NULL)
+
+using namespace std;
+
+int main() {
+    fast_io;
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string copy = s;
+        sort(copy.begin(), copy.end());
+        vector<pair<char,int>> v;
+        char may = 'a';
+        for (int i = n-1; i >= 0; i--) {
+            if (s[i] >= may) {
+                v.emplace_back(s[i], i);
+                may = s[i];
+            }
+        }
+        for (int i = 0; i < v.size(); i++) {
+            s[v[i].second] = v[v.size()-i-1].first;
+        }
+
+        int cont = -1;
+        for (int i = 0 ; i < n; i++) 
+            if (s[i] == v.back().first) cont++;
+
+        cout << ((s == copy) ? (int)v.size() - 1 - cont : -1) << "\n";
+    }
+    return 0;
+}
