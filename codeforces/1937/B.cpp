3659f78e24ac143bf7022418d742b7ef305ae2f8
Angello <angellojamir@hotmail.com>
2024-02-29 14:12:00 -0500

    Add solution for problem `B - Binary Path` Link: https://codeforces.com/contest/1937/problem/B Tags: dp, greedy, implementation Ref: https://codeforces.com/contest/1937/submission/248987020

--- codeforces/1937/B.cpp
+++ codeforces/1937/B.cpp
@@ -9,11 +9,14 @@ char grid[2][200000]{};
 int main() {
     fast_io;
     cin >> tt;
+    vector <vector <bool>> dp(2, vector <bool> (200000, 0));
     while (tt--) {
         cin >> n;
         for (int i = 0; i < 2; i++) 
             for (int j = 0; j < n; j++)
                 cin >> grid[i][j];
+        dp[0][0] = 1; dp[1][n-1] = 1;
+        
         int i = 1;
         for (i; i < n; i++) {
             if(grid[0][i] > grid[1][i-1]) break;
@@ -22,9 +25,12 @@ int main() {
         for (j; j > 0; j--) {
             if(grid[0][j] < grid[1][j-1]) break;
         }
-        for (int ii = 0; ii <= i; ii++) cout << grid[0][ii];
-        for (int jj = i; jj < n; jj++)  cout << grid[1][jj];
-        cout << "\n" << i - j + 1 << " \n";
+        j++;
+        for (int ii = 0; ii <= i; ii++)
+            cout << grid[0][ii];
+        for (int jj = i; jj < n; jj++) 
+            cout << grid[1][jj];
+        cout << "\n" << i - j+ 2<< " \n";
     }
     return 0;
 }
