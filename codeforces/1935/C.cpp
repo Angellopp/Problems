9b83173e23c6c809f1658d175c839013f2f771a9
Angello <angellojamir@hotmail.com>
2024-03-07 13:33:00 -0500

    Add solution for problem `C - Messenger in MAC` Link: https://codeforces.com/contest/1935/problem/C Tags: binary search, brute force, constructive algorithms, data structures, dp, greedy, sortings Ref: https://codeforces.com/contest/1935/submission/250122898

--- codeforces/1935/C.cpp
+++ codeforces/1935/C.cpp
@@ -6,10 +6,11 @@
 #define fast_io ios_base::sync_with_stdio(false); cin.tie (NULL)
 #define readv(v, n) for (int i = 0; i < n; i++) cin >> v[i]
 #define printv(v, n) for (int i = 0; i < n; i++) cout << v[i] << " "; cout << nn
-#define dbug(n) cout << "<[" n.to_string() "] = " << n << ">" << nn
+#define dbug(n) cout << "<[" << #n << "] = " << n << ">" << nn
 using namespace std;
 
 int main() {
+    fast_io;
     int tt;
     cin >> tt;
     while (tt--) {
