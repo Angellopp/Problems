c9b075b813cb105cdee6a7623745788a4044e079
Angello <angellojamir@hotmail.com>
2024-03-08 13:44:00 -0500

    Add solution for problem `A - Too Min Too Max` Link: https://codeforces.com/contest/1934/problem/A Tags: greedy, math Ref: https://codeforces.com/contest/1934/submission/250256254

---
+++ codeforces/1934/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define F first
+#define S second
+#define nn '\n'
+#define fast_io ios_base::sync_with_stdio(false); cin.tie (NULL)
+#define readv(v, n) for (int i = 0; i < n; i++) cin >> v[i]
+#define printv(v, n) for (int i = 0; i < n; i++) cout << v[i] << " "; cout << nn
+#define dbg(n) cout << "<[" << #n << "] = " << n << ">" << nn
+using namespace std;
+
+int main() {
+    fast_io;
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int n;
+        cin >> n;
+        vector <int> v(n);
+        readv(v, n);
+        sort(v.begin(), v.end());
+        cout << 2 * (v[n-1] + v[n-2] - v[0] - v[1]) << nn;
+
+    }
+    return 0;
+}
